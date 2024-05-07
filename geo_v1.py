eca929a49d01c046d18b967d7a4194f8c18a7fed
izawlo <idzabela22@gmail.com>
2024-05-07 12:30:40 +0200

    konflikt

--- geo_v1.py
+++ geo_v1.py
@@ -282,7 +282,7 @@ class Transformacje:
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-dane', type=str, help='Wpisz sciezke do pliku z danymi wejsciowymi')
-    parser.add_argument('-elip', type=str, help='Wybierz elipsoide sposrod dostepnych: WGS84, GRS80, KRASOWSKI')
+    parser.add_argument('-elip',gt type=str, help='Wybierz elipsoide sposrod dostepnych: WGS84, GRS80, KRASOWSKI')
     parser.add_argument('-transf', type=str, help='Wybierz transformacje, z ktorej chcesz skorzystac, sposrod dostepnych: XYZ2flh, flh2XYZ, saz2neu, GK2000, GK1992, XYZ2NEU')
     args = parser.parse_args()
     elip = {'WGS84': [6378137.000, 0.00669438002290], 'GRS80': [6378137.000, 0.00669438002290], 'KRASOWSKI': [6378245.000, 0.00669342162296]}
@@ -307,7 +307,7 @@ if __name__ == '__main__':
         print("Mamy nadzieję, że nasz program był dla Ciebie użyteczny")
 
 
-#
+###
 
         
 
