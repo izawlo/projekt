cda6c1ccbfedd4563756471a05f0a31b025f31d5
alicjawolosiewicz <01179247@pw.edu.pl>
2024-05-07 11:24:39 +0200

    usuniecie niepotrzebnych funkcji

--- geo_v1.py
+++ geo_v1.py
@@ -3,7 +3,7 @@ import sys
 import argparse
 import os
 import tkinter as tk
-
+from numpy import array as np
 
 o = object()
 
@@ -23,9 +23,6 @@ class Transformacje:
         elif model == "grs80":
             self.a = 6378137.0
             self.b = 6356752.31414036
-        elif model == "mars":
-            self.a = 3396900.0
-            self.b = 3376097.80585952
         elif model == "Krasowski":
             self.a = 6378245.000
             self.e2 = 0.00669342162296
@@ -169,77 +166,6 @@ class Transformacje:
         [[E],[N],[U]] = R.T @ xyz_t
         result.append(R.T @ xyz_t)
         return result
-    '''
-    if __name__ == "__main__":
-        # utworzenie obiektu
-        geo = Transformacje(model = "wgs84")
-        print(sys.argv)
-        # dane XYZ geocentryczne
-        X = 3664940.500; Y = 1409153.590; Z = 5009571.170
-        phi, lam, h = geo.xyz2plh(X, Y, Z)
-        print(phi, lam, h)
-        inp_file_path=sys.argv[-1]
-        # phi, lam, h = geo.xyz2plh2(X, Y, Z)
-        # print(phi, lam, h)
-        inp_file_path = sys.argv[-1]
-        
-        # if '--xyz2plh' in sys.argv and '--plh2xyz' in sys.argv:
-        #     print('możesz podać tylko jednej flagę')
-        # elif '--xyz2plh' in sys.argv:
-        #     coords_plh=[]
-        #     with open(inp_file_path) as f:
-                
-        
-    
-    
-    
-    
-    
-    
-        if '--xyz2plh' in sys.argv[-1]
-        coords_plh = []
-        with open ('wsp_inp.txt') as f:
-        lines = f.readlines()
-        lines = lines[4:]
-        for line in lines:
-        line = line.strip()
-        x_str, y_str, z_str = line.split(',')
-        x, y, z =(float(x_str), float(y_str), float(z_str))
-        p, l, h = geo.xyz2plh(x,y,z)
-        coords_plh.append([p,l,h])
-
-        with open('result_xyz2plh.txt', 'w') as f:
-        f.write('phi[deg],lam[phi],h[m]\n')
-        for coords in coords_plh:
-        line = ','.join([str(coord) for coord in coords])
-        f.write(line + '\n')
-        
-        elif '--plh2xyz' in sys.argv:
-            coords_xyz=[]
-            with open ('inp_file_path.txt') as f:
-            lines = f.readlines()
-            lines = lines[1:]
-                for line in lines:
-                line = line.strip()
-                phi_str, lam_str, h_str = line.split(',')
-                phi, lam, h =(float(phi_str), float(lam_str), float(h_str))
-                x, y, z = geo.xyz2plh(phi,lam,h)
-                coords_plh.append([x,y,z])
-
-        with open('result_plh2xyz.txt', 'w') as f:
-        f.write('x[m],y[m],z[m]\n')
-            for coords in coords_plh:
-            line = ','.join([str(coord) for coord in coords])
-            f.write(line + '\n')
-    '''
-        
-    """Tranformacja współrzędnych geocentryczny do współrzędnych topocentrycznych
-    def Rneu(self,f,l):
-        R = np.array([[-np.sin(f)*np.cos(l), -np.sin(l), np.cos(f)*np.cos(l)],
-                      [-np.sin(f)*np.sin(l), np.cos(l), np.cos(f)*np.sin(l)],
-                      [np.cos(f), 0., np.sin(f)]])
-        return(R)
-    """
     
     
     def GK2000(self, f, l, m=0.999923):
